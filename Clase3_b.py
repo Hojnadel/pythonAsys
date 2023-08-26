7966ae918a35d0fabd3985a67eebeb975606944f
Andres Hojnadel <a.hojnadel@gmail.com>
2023-08-26 19:24:03 -0300

    Add script for Class 3 - Duplicated scripts deleted

---
+++ Clase3_b.py
@@ -0,0 +1,52 @@
+#%%
+import my_utils as utl
+utl.clc()
+utl.clearAll()
+
+#%% A: x(t) = exp(-abs(t))
+import my_utils as utl
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+
+dt = 0.001;
+t = np.arange(-20, 20, dt)
+x = np.exp(-np.abs(t))
+
+plt.plot(t,x)
+
+E = utl.ENERGIA(x, dt)
+print("energia: ", E)
+
+
+#%% B: x(t) = sin(2*pi*5*t) + cos(2*pi*10*t)
+PI = np.pi
+dt = 0.001
+To = 0.2
+
+t = np.arange(0, To, dt)
+x = np.sin(2*PI*5*t) + np.cos(2*PI*10*t)
+
+plt.plot(t, x)
+
+pot = utl.POTENCIA(x, dt, To)
+print("Potenica: ", pot)
+
+#%% C: x[n] = 2*(-5/8)^n * u[n]
+
+n = np.arange(0, 15)
+x = 2*(-5/8)**n * utl.u(n)
+
+plt.stem(n,x)
+energia = utl.ENERGIA(x)
+print("Energia: ", energia)
+
+#%% C*: x[n] = 2*(-5/8)^n * u[n-1]
+
+n = np.arange(0, 15)
+x = 2*(-5/8)**n * utl.u(n-1)
+
+plt.stem(n,x)
+energia = utl.ENERGIA(x)
+print("Energia: ", energia)
+# %%
--- my_utils.py
+++ my_utils.py
@@ -1,8 +1,35 @@
 #my_utils.py
 from IPython import get_ipython
+import numpy as np
 
 def clc():
     print("\014")
     
 def clearAll():
     get_ipython().magic('reset -sf')
+
+def ENERGIA(x, dt=1):
+    '''
+    x: signal
+    dt: time-step used to define the signal x. 
+        Default value: dt = 1
+    '''
+    return np.sum(np.abs(x)**2)*dt
+
+def POTENCIA(x, T0, dt=1):
+    '''
+    x: a period (ONLY ONE) of the signal
+    T0: signal period
+    dt: time-step used to define the signal x.
+        Default value: dt = 1
+    '''
+    return 1/T0 * np.sum(np.abs(x)**2)*dt
+
+#Definición de la función IMPULSO UNITARIO APROXIMADO delta(t)
+delta = lambda t,Ts: np.piecewise(t, t==0, [1/Ts,0])
+
+#Definición de la función ESCALÓN UNITARIO u(t)
+u = lambda t: np.piecewise(t, t>=0, [1,0])
+
+#Definición de la función RAMPA UNITARIA rho(t)
+rho = lambda t: t*u(t)
